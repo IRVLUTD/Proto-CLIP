c91cd1ba1f2c42c29c8deffa8af9f306b552f6b3
Kamalesh Palanisamy <kamalesh800@gmail.com>
2023-12-15 21:44:28 -0500

    Fix pip install issues and upgrade to 0.2

--- toolkit/setup.py
+++ toolkit/setup.py
@@ -2,17 +2,22 @@ from pathlib import Path
 import setuptools
 from setuptools import setup
 
-required = Path("requirements.txt").read_text(encoding="utf-8").split("\n")
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+required = (this_directory / "requirements.txt").read_text().split("\n")
+
 
 setup(name='proto_clip_toolkit', 
       description="A simple toolkit from Proto-CLIP demo that provies speech recognition, part-of-speech tagging and realworld robot demo APIs.",
-      author="IRVL-UTd: Intelligent Robotics and Vision Lab at the University of Texas at Dallas",
+      author="IRVL-UTD: Intelligent Robotics and Vision Lab at the University of Texas at Dallas",
       author_email="irvl.utd@gmail.com",
-      version='0.1',
+      version='0.2',
+      install_requires=required,
       url="https://github.com/IRVLUTD/Proto-CLIP",
       license="MIT",
       packages=setuptools.find_packages(),
-      install_requires=required,
       include_package_data=True,
-      python_requires=">=3.7"
+      python_requires=">=3.7",
+      long_description=long_description,
+    long_description_content_type='text/markdown'
 )
